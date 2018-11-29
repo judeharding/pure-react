6748b14d6a64f66a5bf200f291d3f8f9c96a4e70
judeharding <jharding@genesisgroupusa.com>
2018-11-29 14:40:14 -0500

    pure react inital

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function HelloWorld() {
+    return (
+        <div>
+            <Hello />
+            <World /> !
+            <NameCells />
+            raw text no quote
+        </div>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Hello() {
+    return (
+        <span>Hello</span>
+    );
+}
+
+function World() {
+    return (
+        <span>World!!</span>
+    );
+}
+
+function NameCells() {
+    return (
+        <React.Fragment> <td>First Name</td> <td>Last Name</td>
+        </React.Fragment>
+    );
+}
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.querySelector("#root")
+);
