69d9972e6a51429130cdf0efd4292d686912338f
judeharding <jharding@genesisgroupusa.com>
2018-12-18 13:28:19 -0500

    working on pg 83 84

--- src/index.js
+++ src/index.js
@@ -2,19 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// ex1
 
-function AddressLabel(person) {
+// function AddressLabel(person) {
+//     return (
+//         <span>
+//             Full Name <br />
+//             123 Fake St.<br />
+//             Boston, MA 02118
+//         </span>
+//     );
+// }
+
+// //AddressLabel to the index html
+// ReactDOM.render(
+//     <AddressLabel person={AddressLabel} />, document.querySelector('#root')
+// );
+
+// ex2
+
+function Stamp({ hash }) {
+    var url = `https://www.gravatar.com/avatar/${hash}`;
+    return (
+        <img src={url} className="stamp" alt="stamp" />
+    );
+}
+
+function ToPerson(toPerson) {
     return (
         <span>
-            Full Name <br />
+            TO Name <br />
             123 Fake St.<br />
             Boston, MA 02118
         </span>
     );
 }
 
+function FromPerson(fromPerson) {
+    return (
+        <span>
+            FROM Name <br />
+            123 Fake St.<br />
+            Boston, MA 02118
+        </span>
+    );
+}
+function Envelope(toPerson, fromPerson) {
+    return (
+        FromPerson,
+        ToPerson,
+        Stamp
+    )
+}
 
-//Tweet to the index html
+//Envelope to the index html
 ReactDOM.render(
-    <AddressLabel person={AddressLabel} />, document.querySelector('#root')
+    <Envelope label={Envelope} />, document.querySelector('#root')
 );
+
+
