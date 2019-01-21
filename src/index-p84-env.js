ada5abc998ca8d8013f1f0989f7d5f3b8c1a4c6b
judeharding <jharding@genesisgroupusa.com>
2019-01-21 12:28:19 -0500

    starting card exercise

---
+++ src/index-p84-env.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PropTypes from 'prop-types';
+
+// ex1
+
+// function AddressLabel(person) {
+//     return (
+//         <span>
+//             Full Name <br />
+//             123 Fake St.<br />
+//             Boston, MA 02118
+//         </span>
+//     );
+// }
+
+// //AddressLabel to the index html
+// ReactDOM.render(
+//     <AddressLabel person={AddressLabel} />, document.querySelector('#root')
+// );
+
+// ex2
+
+function Stamp({ hash }) {
+    var url = `https://www.gravatar.com/avatar/${hash}`;
+    return (
+        <img src={url} className="stamp" alt="stamp" />
+    );
+}
+
+function ToPerson(toPerson) {
+    return (
+        <div className="to">
+            <span>
+                TO Name <br />
+                123 Fake St.<br />
+                Boston, MA 02118
+            </span>
+        </div>
+    );
+}
+
+function FromPerson(fromPerson) {
+    return (
+        <div className="from">
+            <span>
+                FROM Name <br />
+                123 Fake St.<br />
+                Boston, MA 02118
+            </span>
+        </div>
+    );
+}
+
+//REQUIRING Components and the data expected (example)
+FromPerson.propTypes = {
+    from: PropTypes.string.isRequired
+}
+
+
+function Envelope(toPerson, fromPerson) {
+    return (
+        <div className="envelope">
+            <FromPerson />
+            <ToPerson />
+            <Stamp />
+        </div >
+    )
+}
+
+
+//Envelope to the index html
+ReactDOM.render(
+    <Envelope label={Envelope} />, document.querySelector('#root')
+);
+
