4b9c66cdd5be13dde4bffbfef868c306d4c12d29
judeharding <jharding@genesisgroupusa.com>
2019-02-13 19:23:05 -0500

    exercise 4 start

---
+++ src/index-p85-cc.js
@@ -0,0 +1,125 @@
+// Their WORKING code
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+var cardInfo = {
+    bankName: "Big Bank, Inc.",
+    cardNumber: "1234 5678 8765 4321",
+    expirationDate: "8/19",
+    name: "Cardholder Name"
+};
+
+CreditCard.propTypes = {
+    card: PropTypes.shape({
+        bankName: PropTypes.string.isRequired,
+        cardNumber: PropTypes.string.isRequired,
+        expirationDate: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired
+    }).isRequired
+};
+
+function CreditCard({ card }) {
+    var { name, cardNumber, expirationDate, bankName } = card;
+    return (
+        <div className="credit-card">
+            <div className="bank-name">{bankName}</div>
+            <div className="card-number">{cardNumber}</div>
+            <div className="first-four-digits">{cardNumber.substring(0, 4)}</div>
+            <div className="expiration">
+                <span className="valid-thru">Valid Thru</span> {expirationDate}
+            </div>
+            <div className="name">{name}</div>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <CreditCard card={cardInfo} />,
+    document.getElementById('root')
+);
+
+
+
+
+
+
+// MY CODE
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// // import PropTypes from 'prop-types';
+
+
+// // ex3 credit card exercise
+
+
+// function BankName(bankName) {
+//     return (
+//         <div className="bank">
+//             <span>
+//                 Big Bank, Inc. <br />
+//             </span>
+//         </div>
+//     );
+// }
+
+// function CcNumber(ccNumber) {
+//     return (
+//         <div className="ccnumber">
+//             <span>
+//                 1234 5678 9101 2134 <br />
+//             </span>
+//         </div>
+//     );
+// }
+
+// // //REQUIRING Components and the data expected (example)
+// // CcNumber.propTypes = {
+// //     ccNumber: PropTypes.number.isRequired
+// // }
+
+// function ExpDate(expDate) {
+//     return (
+//         <div className="date">
+//             <span>
+//                 Valid Thru<br />
+//                 01/19 <br />
+//             </span>
+//         </div>
+//     );
+// }
+
+// function PersonName(personName) {
+//     return (
+//         <div className="name">
+//             <span>
+//                 CARDHOLDER NAME <br />
+//             </span>
+//         </div>
+//     );
+// }
+
+
+
+// function CreditCard(personName, expDate, ccNumber, bankName) {
+//     return (
+//         <div className="card">
+//             <PersonName />
+//             <ExpDate />
+//             <CcNumber />
+//             <BankName />
+//         </div >
+//     )
+// }
+
+
+
+// ReactDOM.render(
+//     <CreditCard label={CreditCard} />, document.querySelector('#root')
+// );
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-// Their WORKING code
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function CreditCard({ card }) {
-    var { name, cardNumber, expirationDate, bankName } = card;
+var cardInfo = {
+    bankName: "Big Bank, Inc.",
+    cardNumber: "1234 5678 8765 4321",
+    expirationDate: "8/19",
+    name: "Cardholder Name"
+};
+
+function Logo({ hash }) {
+    var url = `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwit-Kbw77ngAhUQ16wKHed1DBgQjRx6BAgBEAU&url=https%3A%2F%2Ffacebook.github.io%2Fcreate-react-app%2Fdocs%2Fadding-images-fonts-and-files&psig=AOvVaw0cTBsbOlpNSgy7BgtTxRu_&ust=1550187307767404{hash}`;
     return (
-        <div className="credit-card">
-            <div className="bank-name">{bankName}</div>
-            <div className="card-number">{cardNumber}</div>
-            <div className="first-four-digits">{cardNumber.substring(0, 4)}</div>
-            <div className="expiration">
-                <span className="valid-thru">Valid Thru</span> {expirationDate}
-            </div>
-            <div className="name">{name}</div>
-        </div>
+        <img src={url} className="logo" alt="logo" />
     );
 }
 CreditCard.propTypes = {
@@ -28,103 +25,22 @@ CreditCard.propTypes = {
     }).isRequired
 };
 
-var cardInfo = {
-    bankName: "Big Bank, Inc.",
-    cardNumber: "1234 5678 8765 4321",
-    expirationDate: "8/19",
-    name: "Cardholder Name"
-};
+function CreditCard({ card }) {
+    var { name, cardNumber, expirationDate, bankName } = card;
+    return (
+        <div className="poster">
+            <div className="bank-name">{bankName}</div>
+            <div className="logo">{cardNumber}</div>
+            <div className="first-four-digits">{cardNumber.substring(0, 4)}</div>
+            <div className="expiration">
+                <span className="valid-thru">Valid Thru</span> {expirationDate}
+            </div>
+            <div className="name">{name}</div>
+        </div>
+    );
+}
 
 ReactDOM.render(
     <CreditCard card={cardInfo} />,
     document.getElementById('root')
 );
-
-
-
-
-
-
-
-
-
-
-
-
-// MY CODE
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// // import PropTypes from 'prop-types';
-
-
-// // ex3 credit card exercise
-
-
-// function BankName(bankName) {
-//     return (
-//         <div className="bank">
-//             <span>
-//                 Big Bank, Inc. <br />
-//             </span>
-//         </div>
-//     );
-// }
-
-// function CcNumber(ccNumber) {
-//     return (
-//         <div className="ccnumber">
-//             <span>
-//                 1234 5678 9101 2134 <br />
-//             </span>
-//         </div>
-//     );
-// }
-
-// // //REQUIRING Components and the data expected (example)
-// // CcNumber.propTypes = {
-// //     ccNumber: PropTypes.number.isRequired
-// // }
-
-// function ExpDate(expDate) {
-//     return (
-//         <div className="date">
-//             <span>
-//                 Valid Thru<br />
-//                 01/19 <br />
-//             </span>
-//         </div>
-//     );
-// }
-
-// function PersonName(personName) {
-//     return (
-//         <div className="name">
-//             <span>
-//                 CARDHOLDER NAME <br />
-//             </span>
-//         </div>
-//     );
-// }
-
-
-
-// function CreditCard(personName, expDate, ccNumber, bankName) {
-//     return (
-//         <div className="card">
-//             <PersonName />
-//             <ExpDate />
-//             <CcNumber />
-//             <BankName />
-//         </div >
-//     )
-// }
-
-
-
-// ReactDOM.render(
-//     <CreditCard label={CreditCard} />, document.querySelector('#root')
-// );
-
-
-
