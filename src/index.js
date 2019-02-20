044791304f8ed614a120def8ecabdacfefe2ac11
judeharding <jharding@genesisgroupusa.com>
2019-02-20 13:24:53 -0500

    working on a basic template for me

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
+
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; //for data validation
 import ReactDOM from 'react-dom';
 import './index.css';
 
+var varInfo = {
+    keyOne: "Big Bank, Inc.",
+    keyTwo: "1234 5678 8765 4321",
+    keyThree: "8/19",
+    keyFour: "Cardholder Name"
+};
 
+// data validation
+ComponentName.propTypes = {
+    prop: PropTypes.shape({
+        keyOne: PropTypes.string.isRequired,
+        keyTwo: PropTypes.string.isRequired,
+        keyThree: PropTypes.string.isRequired,
+        keyFour: PropTypes.string.isRequired
+    }).isRequired
+};
 
-Function Xxxx({ testBtn }) {
+function ComponentName({ prop }) {
+    var { keyOne, keyTwo, keyThree, keyFour } = prop;
+    //MUST HAVE A RETURN
     return (
-        <div>
-            {btn}
+        // Could return below but it would be without css styling and it is only one element
+        // Make sure you wrap your return in span or div or React.Fragment tags to make it 1 thing
+        // return <span> <ComponentName /> </span>
+
+        //CAN ONLY RETURN 1 THING
+        <div className="box">
+            <div className="bank-name">{keyOne}</div>
+            <div className="card-number">{keyTwo}</div>
+            <div className="first-four-digits">{keyTwo.substring(0, 4)}</div>
+            <div className="expiration">
+                <span className="valid-thru">Valid Thru</span> {keyThree}
+            </div>
+            <div className="name">{keyFour}</div>
         </div>
     );
 }
 
-var testBtn = {
-    < IconButton > Do The Thing</IconButton>
-}
+// you can insert real JS expressions within JSX by wrapping it in braces
+// you can return an IF stmt by using terniary <span>{isValid ? 'valid' : 'not valid'}</span>
 
-
-ReactDOM.render(<Xxxx btn={testBtn} />,
+//renders everything above to the index.html
+ReactDOM.render(
+    <ComponentName prop={varInfo} />,
     document.getElementById('root')
 );
+
+
